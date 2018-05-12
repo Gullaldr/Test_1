a500f4d43a309e78d3a495e37d8b1d9444e88065
NukeBird <nukebird.dev@gmail.com>
2018-05-12 22:23:22 +0300

    Add rotation to monkey head

---
+++ test_1/228/Assets/Some.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Some : MonoBehaviour {
+
+	Transform t;
+
+	// Use this for initialization
+	void Start () 
+	{
+		t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		t.Rotate(Vector3.up * 90f * Time.deltaTime, Space.World);
+	}
+}
